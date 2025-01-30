d9fcea21dfd1d41afc603872fd9f4b2cbccf053f
kaiquesousa2005 <kaique.freire@hotmail.com>
2025-01-30 18:16:07 -0300

    Finalização Etapa 1 firebase

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBb47gvzVDUeHw2IpMt5M7g_0EcKmv1OrM",
-  authDomain: "loja-da-manu-ccec6.firebaseapp.com",
-  projectId: "loja-da-manu-ccec6",
-  storageBucket: "loja-da-manu-ccec6.firebasestorage.app",
-  messagingSenderId: "1002360825453",
-  appId: "1:1002360825453:web:ee294aabca232cb25f4164",
-  measurementId: "G-1VNYJYFESF",
-}
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  };
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
